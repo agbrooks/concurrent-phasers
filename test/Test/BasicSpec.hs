0bb5c73a0e54b87544aa63b1d06b30f09e3c32a8
Andrew Brooks <andrewgrantbrooks@gmail.com>
2017-10-15 21:57:14 -0500

    Added unit tests (some currently fail on deadlock)

---
+++ test/Test/BasicSpec.hs
@@ -0,0 +1,72 @@
+module Test.BasicSpec
+  ( spec
+  , main )
+where
+
+import Control.Concurrent ( forkIO )
+import Control.Monad      ( (=<<)
+                          , forM_ )
+import Test.Hspec
+import Test.QuickCheck
+
+import Control.Concurrent.Phaser
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "Basic Phaser" $ do
+    it "Permits registration" $ do
+      ph <- newIntPhaser 0
+      forM_ [1..100]
+        (\i -> do
+          register ph
+          num_registered <- registered ph
+          num_registered `shouldBe` i
+        )
+
+    it "Permits deregistration" $ do
+      ph <- newIntPhaser 100
+      forM_ (reverse [0..99])
+        (\i -> do
+          unregister ph
+          num_registered <- registered ph
+          num_registered `shouldBe` i
+        )
+
+    it "Provides phase when requested" $ do
+      -- Int for phase
+      the_phase <- phase =<< newPhaser (0 :: Int) 0
+      the_phase `shouldBe` 0
+      -- Integer for phase
+      the_phase <- phase =<< newPhaser (11 :: Integer) 0
+      the_phase `shouldBe` 11
+      -- Ordering for phase (why not?)
+      the_phase <- phase =<< newPhaser LT 0
+      the_phase `shouldBe` LT
+
+    it "Advances with one registered party" $ do
+      ph <- newIntPhaser 1
+      num_registered <- registered ph
+      num_registered `shouldBe` 1
+
+      forM_ [1..100]
+        (\i -> do
+          await ph
+          new_phase <- phase ph
+          new_phase `shouldBe` i
+        )
+
+    it "Advances with multiple registered parties" $ do
+      ph <- newIntPhaser 2
+      num_registered <- registered ph
+      num_registered `shouldBe` 2
+
+      forM_ [1..100]
+        (\i -> do
+          forkIO (await ph)
+          await ph
+          new_phase <- phase ph
+          new_phase `shouldBe` i
+        )
