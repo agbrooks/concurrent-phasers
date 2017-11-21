1020535e4dce381b27e27bc6951544b668c53043
Andrew Brooks <andrewgrantbrooks@gmail.com>
2017-11-20 20:54:05 -0600

    Added exception-safe `Countdown` draft

--- src/Control/Concurrent/Phaser/Internal.hs
+++ src/Control/Concurrent/Phaser/Internal.hs
@@ -1,8 +1,9 @@
 module Control.Concurrent.Phaser.Internal
 where
 
+import Control.Concurrent      ( forkIO )
 import Control.Concurrent.MVar
-
+import Control.Monad           ( when )
 
 {- |
   A one-shot @Countdown@ amongst threads.
@@ -17,18 +18,27 @@ import Control.Concurrent.MVar
 -}
 data Countdown = Countdown
   { registered  :: MVar Int -- ^ Number of parties registered. Should not drop
-                             --   below zero.
+                            --   below zero.
   , arrived     :: MVar Int -- ^ Number of parties arrived. This value is not
-                               --   reset unless reset explicitly by the
-                               --  on_completion action.
+                            --   reset unless reset explicitly by the
+                            --  on_completion action.
   , on_completion :: IO () -- ^ An effect triggered on completion of the
-                            --   countdown.
+                           --   countdown.
   }
+
+-- | Set the number of parties arrived on a @Countdown@ to zero. If a number of
+--   parties arrived is already present, this will block.
+reset :: Countdown -> IO ()
+reset c = putMVar (arrived c) 0
 
 -- | Create a new @Countdown@ for 'i' parties that executes callback 'callback'.
 --   'i' may not drop below zero.
 newCountdown :: Int -> IO () -> IO Countdown
-newCountdown i callback = newDisabledCountdown i callback >>= reset
+newCountdown i callback
+  = Countdown
+  <$> newMVar (max i 0)
+  <*> newMVar 0
+  <*> return callback
 
 -- | Create a new @Countdown@ with 'i' parties that executes callback 'callback'.
 --   'i' may not drop below zero. Any attempts to arrive at the @Countdown@ will
@@ -40,9 +50,9 @@ newDisabledCountdown i callback
   <*> newEmptyMVar
   <*> return callback
 
--- | "Reset" a Countdown, setting the number of arrived parties to zero.
-reset :: Countdown -> IO Countdown
-reset c = putMVar (arrived c) 0 >> return c
+-- | Replace the action performed by a Countdown
+withNewAction :: Countdown -> IO () -> Countdown
+withNewAction (Countdown r a _) f = Countdown r a f
 
 -- | Register a new party at a @Countdown@.
 registerCountdown :: Countdown -> IO ()
@@ -52,16 +62,33 @@ registerCountdown c = modifyMVar_ (registered c)
 -- | Arrive, but unregister a party at a @Countdown@. The registration count may
 --   not go below zero.
 unregisterArriveCountdown :: Countdown -> IO ()
-unregisterArriveCountdown c = modifyMVar_ (registered c)
-  (\r -> return $ max 0 (r - 1))
-  -- FIXME: Should determine whether or not the countdown should advance.
+unregisterArriveCountdown c =
+  modifyMVar_ (registered c)
+  (\n_reg -> do
+      withMVar (arrived c)
+       (\n_arr -> do
+          when (n_reg - 1 <= n_arr) $
+            runCallback c
+       )
+      return $ max 0 (n_reg - 1)
+  )
 
--- XXX: Make sure it's clear that the value taken is NOT restored...
+-- | Arrive at a @Countdown@. If we are the last party to arrive, run the
+--   callback and do not replaced the 'arrived' counter.
 arriveCountdown :: Countdown -> IO ()
-arriveCountdown c = undefined
+arriveCountdown c =
+  withMVar (registered c)
+  (\n_reg ->
+      modifyMVar_ (arrived c)
+      (\n_arr -> do
+          when (n_arr >= n_reg - 1) $
+            runCallback c
+          return (n_arr + 1)
+      )
+  )
 
--- | Run the action associated with a @Countdown@.
+-- | Run the action associated with a @Countdown@'s completion in a new thread.
 --   This operation is strict.
 runCallback :: Countdown -> IO ()
-runCallback c = seq (on_completion c) (return ())
+runCallback c = seq (forkIO $ on_completion c) (return ())
 {-# INLINABLE runCallback #-}
