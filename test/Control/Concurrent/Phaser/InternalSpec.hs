c39138d81c9c2704ddd0ec32ac30d83130b542c1
Andrew Brooks <andrewgrantbrooks@gmail.com>
2017-11-21 00:11:22 -0600

    Fleshed out internal tests a bit more

--- test/Control/Concurrent/Phaser/InternalSpec.hs
+++ test/Control/Concurrent/Phaser/InternalSpec.hs
@@ -11,7 +11,8 @@ import Control.Concurrent ( forkIO
                           , yield )
 import Control.Monad      ( (=<<)
                           , forM_ )
-
+       
+import Data.Time.Clock
 import Test.Hspec
 import Test.QuickCheck
 
@@ -23,6 +24,7 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "Countdown" $ do
+
     it "Performs registration" $ do
       cd <- newCountdown 1 (return ())
       forM_ [1..100]
@@ -31,3 +33,32 @@ spec = do
           num_registered <- readMVar $ registered cd
           num_registered `shouldBe` (i + 1)
         )
+
+    it "Performs unregistration" $ do
+      cd <- newCountdown 101 (return ())
+      forM_ [1..100]
+        (\i -> do
+            unregisterArriveCountdown cd
+            num_registered <- readMVar $ registered cd
+            num_registered `shouldBe` (101 - i)
+        )
+
+    --it "Can advance" $ do
+      --cd <- newCountdown 1 (return ())
+      --arriveCountdown
+
+reattemptFor :: Double -> IO Bool -> IO Bool
+reattemptFor interval action =
+  let dt = picosecondsToDiffTime (round $ interval * 1e+12)
+  in getCurrentTime >>= (\now ->
+    let deadline = addUTCTime ((fromRational . toRational) dt) now
+    in action `byDeadline` deadline
+  )
+  where
+    action `byDeadline` deadline = do
+      yield
+      result <- action
+      now    <- getCurrentTime
+      if | result         -> return True
+         | now > deadline -> return False
+         | otherwise      -> action `byDeadline` deadline
