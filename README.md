7e0a58603fd6b6e48d69df6f59ec11ffbb0bcd51
Andrew Brooks <andrewgrantbrooks@gmail.com>
2018-04-25 21:44:29 -0500

    Initial STM rewrite
    - Rewrote Phaser to use STM and have Habanero-like semantics
      (It's significantly easier to reason about now.)
    
    - Minor README.md updates

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-# concurrent-phasers
+# concurrent-phasers (INCOMPLETE)
 
-This package implements "Phasers" in the style of Java's [java.util.concurrent.Phaser](https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/Phaser.html) class. 
+## What's this?
 
-Currently, it is a work in progress and is therefore unsuitable for production use.
+This package is a Haskell implementation of the "Phaser," a barrier-like, 
+point-to-point synchronization object. Its construction and behavior is inspired
+by the object of the same name that is part of the 
+[Habanero Project](https://habenero.rice.edu).
+
+The `Phaser` type is semantically different from, but not entirely unlike, Java's
+[java.util.concurrent.Phaser](https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/Phaser.html) class. 
+
+Currently, it is a work in progress and should not be used in a production setting.
+
+## Planned Features
+
+* Deadlock-resistance for asynchronous exceptions
+
+* Tree-structuring of Phasers to eliminate livelock for large numbers of
+processes
+
+
--- src/Control/Concurrent/Phaser.hs
+++ src/Control/Concurrent/Phaser.hs
@@ -1,224 +1,186 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE MultiWayIf   #-}
 module Control.Concurrent.Phaser
-  ( newPhaser
+  ( Phaser ()
+  , PhaserMode
+  , newPhaser
   , newIntPhaser
   , phase
-  , registered
-  , arrived
-  , await
-  , awaitFor
-  , awaitUntil
   , register
---  , signal
---  , lurk
---  , lurkFor
---  , lurkUntil
   , unregister
-  , Phaser () )
+  , batchRegister
+  , batchUnregister
+  , runPhased
+  , runMultiPhased )
 where
 
-import Control.Applicative     ( (<|>) )
-import Control.Concurrent
-import Control.Concurrent.MVar
-import Control.Exception ( bracketOnError
-                         , onException
-                         , uninterruptibleMask_ )
-import Control.Monad           ( (>=>)
-                               , join
-                               , liftM
-                               , when )
-import Data.Maybe ( isNothing )
+import Control.Concurrent.STM
+import Control.Exception       ( uninterruptibleMask_ )
+import Control.Monad           ( when )
 
-{-
-TODO: Ensure that we can do `\`onExcept\` unregister` nonsense by adjusting
-      the Countdown structure.
-TODO (LT): Add support for tree-structured phasers to reduce lock contention.
-TODO (LT): Add more documentation.
+{- TODO:
+* Check Haddock syntax.
+* Apply uninterruptableMask_ to prevent deadlock.
 -}
 
-type SleepQueue = [MVar ()]
-
-{-|
-A "Phaser" acts as a reusable barrier with an adjustable number of thread
-that it synchronizes.
+{- |Mode in which a task uses a @Phaser@.
+    A task in `Wait` mode on a @Phaser@ will be blocked when trying to exit the
+    @Phaser@ until all tasks in `Signal` mode have tried to leave.
+    `SignalWait` combines `Signal` and `Wait` modes.
 -}
+data PhaserMode
+  = Signal
+  | Wait
+  | SignalWait
+
 data Phaser p = Phaser
-  { _phase      :: !(MVar p)    -- ^ Current phase of the phaser
-  , _arrived    :: !(MVar Int)  -- ^ Number of parties arrived at the phaser
-  , _registered :: !(MVar Int)  -- ^ Number of parties registered at the phaser
-  , _wake_queue :: MVar SleepQueue -- ^ A list of MVars to unblock when the phaser
-                                   -- ^ advances.
+  { _phase       :: TVar p   -- ^ Phase of the phaser.
+  , _registered  :: TVar Int -- ^ Parties registered on the phaser.
+  , _adjustment  :: TVar Int -- ^ Number of parties planning to enter or leave
+                             --   during the next @Phaser@ pass.
+  , _sig_rx   :: TMVar Int   -- ^ Signals received.
+  , _sig_reg  :: TVar Int    -- ^ Signals registered (signals required to advance)
+  , _wait_fin :: TMVar Int -- ^ Waits finished.
+  , _wait_reg :: TVar Int  -- ^ Waits registered.
+  , _entered  :: TMVar Int -- ^ Parties that have entered the phaser.
   }
 
--- | Read the "phase" of the Phaser. Once all threads advance, the phase
---   increases.
+-- | Create a new @Phaser@.
+newPhaser
+  :: Enum p
+  => p   -- ^ Starting phase for the @Phaser@.
+  -> Int -- ^ Number of parties to expect for first round.
+  -> IO (Phaser p)
+newPhaser p parties =
+  atomically $
+  Phaser <$> newTVar  p       -- Initial phase
+         <*> newTVar  parties -- Initial parties
+         <*> newTVar  0
+         <*> newEmptyTMVar  -- Signals received, empty until all register.
+         <*> newTVar  0
+         <*> newEmptyTMVar  -- Waits received, empty until all register.
+         <*> newTVar  0
+         <*> newTMVar 0 -- Number entered. Zero for now.
+
+-- | Create a new phaser with initial @Phase@ 0.
+newIntPhaser :: Int -> IO (Phaser Int)
+newIntPhaser parties = newPhaser 0 parties
+
+-- | Retrieve a @Phaser@'s current phase.
 phase :: Phaser p -> IO p
-phase = readMVar . _phase
+phase = atomically . readTVar . _phase
 
--- | Determine how many parties / threads are currently registered with the
---   @Phaser@.
-registered :: Phaser p -> IO Int
-registered = readMVar . _registered
+-- | Tell a @Phaser@ to expect one more party when its next phase begins.
+register :: Phaser p -> IO ()
+register p = batchRegister p 1
 
--- | Determine how many parties have arrived at the @Phaser@.
-arrived :: Phaser p -> IO Int
-arrived = readMVar . _arrived
+-- | Tell a @Phaser@ to expect one fewer party when its next phase begins.
+unregister :: Phaser p -> IO ()
+unregister p = batchUnregister p (-1)
 
-{- |
-  Create a new @Phaser@. Note that a phaser may have no fewer than 0 parties
-  registered.
+-- | Register several parties on a @Phaser@.
+batchRegister :: Phaser p -> Int -> IO ()
+batchRegister p i = atomically $ modifyTVar (_adjustment p) (+i)
+
+-- | Unregister several parties on a @Phaser@.
+batchUnregister :: Phaser p -> Int -> IO ()
+batchUnregister p i = batchRegister p (-i)
+
+{- |Synchronize a task with a @Phaser@.
+    Caller is responsible for graceful unregistration in the event of an
+    asynchronous exception.
 -}
-newPhaser :: Enum p
-          => p -- ^ @phase@ to start in.
-          -> Int -- ^ Number of parties to initially register with the @Phaser@.
-          -> IO (Phaser p)
-newPhaser p i =
-  Phaser <$>
-    newMVar p <*>
-    newMVar 0 <*>
-    newMVar i <*>
-    newMVar []
+runPhased
+  :: Enum p
+  => Phaser p   -- ^@Phaser@ in question
+  -> PhaserMode -- ^Mode the task will use when synchronizing on the @Phaser@
+  -> IO a       -- ^Task performed
+  -> IO a
+runPhased p m action = do
+  p `enterInMode` m
+  result <- action
+  when (modeSignals m) $ signal p
+  when (modeWaits   m) $ wait   p
+  return result
 
--- | Create a new @Phaser@ which uses an Int to track @phase@, starting at phase 0.
-newIntPhaser
-  :: Int -- ^ Number of parties to initially register with the Phaser.
-  -> IO (Phaser Int)
-newIntPhaser = newPhaser 0
-
--- | Register a thread with a @Phaser@.
-register :: Enum p => Phaser p -> IO ()
-register ph = modifyMVar_ (_registered ph) (\i -> return (i+1))
-
-{- |
-  Immediately unregister from a phaser. If this would cause the registered count
-  to drop below zero, then the registered count will not change. If this would
-  cause all requisite parties to be considered "registered", then advance the
-  phaser.
+{- |Synchronize a task across multiple @Phaser@s in multiple @PhaserMode@s.
+    The @Phaser@/@PhaserMode@ pairs may be specified in any order.
 -}
-unregister :: Enum p => Phaser p -> IO ()
-unregister ph =
-  modifyMVarMasked_ (_registered ph)
-  (\n_reg ->
-     modifyMVarMasked_ (_arrived ph)
-       (\n_arr ->
-         let
-           any_registered = n_reg > 0
-           is_done = n_arr > n_reg - 1
-         in
-           if (any_registered && is_done) then uninterruptibleMask_ $ do
-             nextPhase ph
-             forkIO $ awaken ph
-             return 0
-           else do
-             return n_arr
-     )
-     return $ max 0 (n_reg - 1)
-  )
+-- | Synchronize a task across multiple @Phaser@s in multiple @PhaserMode@s.
+runMultiPhased :: Enum p => [(Phaser p, PhaserMode)] -> IO a -> IO a
+runMultiPhased [] action = action
+runMultiPhased pairs action =
+  let signal_pairs = filter (\(_, m) -> modeSignals m) pairs
+      wait_pairs   = filter (\(_, m) -> modeWaits   m) pairs
+  in do
+    mapM_ (uncurry enterInMode) signal_pairs
+    mapM_ (uncurry enterInMode) wait_pairs
+    result <- action
+    mapM_ (signal . fst) signal_pairs
+    mapM_ (wait   . fst) wait_pairs
+    return result
 
-sleepOn :: SleepQueue -> IO (SleepQueue, MVar ())
-sleepOn queue = do
-  block <- newEmptyMVar
-  let queue' = block:queue
-  return (queue', block)
+{- Internal functions follow -}
 
+-- Does a given mode indicate that we should signal?
+modeSignals :: PhaserMode -> Bool
+modeSignals Wait = False
+modeSignals _    = True
 
--- | Wait at the phaser until all threads arrive. Once all threads have arrived,
--- | the @Phaser@ proceeds to the next phase, and all threads are unblocked.
-await :: Enum p => Phaser p -> IO ()
-await ph =
-  -- FIXME: Put registered back before blocking, please.
+-- Does a given mode indicate that we should wait?
+modeWaits :: PhaserMode -> Bool
+modeWaits Signal = False
+modeWaits _      = True
 
-  modifyMVarMasked_ (_registered ph)
-  (\n_reg ->
-     bracketOnError
-       (takeMVar $ _arrived ph)
-       (\n_arr -> tryPutMVar (_arrived ph) n_arr)
-       (\n_arr ->
-          let is_done = n_arr > n_reg - 1 in
-            if is_done then uninterruptibleMask_ $ do
-              nextPhase ph
-              forkIO $ awaken ph
-              putMVar (_arrived ph) 0
-            else do
-              block <- modifyMVar (_wake_queue ph) sleepOn
-              putMVar (_arrived ph) (n_arr + 1)
-              (takeMVar block) `onException` (modifyMVar (_arrived ph) (-1))
-       )
-  )
+-- Alter the number of registered parties by adding the adjustment.
+applyAdjustment :: Phaser p -> STM ()
+applyAdjustment p = do
+  adj <- swapTVar (_adjustment p) 0
+  modifyTVar (_registered p) (+ adj)
 
--- | @await@ a @Phaser@ for a specified number of phases.
-awaitFor
-  :: Enum p
-  => Int -- ^ Number of phases to wait for
-  -> Phaser p -- ^ Phaser to wait on
-  -> IO ()
-awaitFor 0 ph = return ()
-awaitFor i ph = await ph >> awaitFor (i - 1) ph
+-- Enter the phaser in a given mode.
+enterInMode :: Phaser p -> PhaserMode -> IO ()
+enterInMode p m = atomically $ do
+  -- Adjust for new parties registered or unregistered
+  entered    <- takeTMVar (_entered p)
+  when (entered == 0) $ applyAdjustment p
+  registered <- readTVar (_registered p)
 
--- | @await@ a @Phaser@ until it reaches a certain phase.
-awaitUntil
-  :: Enum p
-  => Eq p
-  => p        -- ^ Phase to wait for
-  -> Phaser p -- ^ @Phaser@ to wait on
-  -> IO ()
-awaitUntil target_phase phaser =
-  phase phaser >>= (\current_phase -> do
-    when (current_phase /= target_phase) $
-      await phaser >> awaitUntil target_phase phaser
-    return ()
-  )
+  -- Adjust signal / wait registration
+  when (modeSignals m) $ modifyTVar (_sig_reg  p) (+1)
+  when (modeWaits   m) $ modifyTVar (_wait_reg p) (+1)
 
--- | Count the thread among those arriving at the phaser, but don't block
---   waiting for any others to arrive. A thread which signals the @Phaser@
---   should not `lurk` on the @Phaser@.
+  -- If all expected parties arrived, unblock their exit.
+  if entered + 1 >= registered then finishEntry p
+  else putTMVar (_entered p) (entered + 1)
+
+-- Unblock exit of signalling parties. These, in turn, free the waiting parties.
+finishEntry :: Phaser p -> STM ()
+finishEntry p = putTMVar (_sig_rx p) 0
+
+wait :: Enum p => Phaser p -> IO ()
+wait p = atomically $ do
+  w_reg <- readTVar  (_wait_reg p)
+  w_fin <- takeTMVar (_wait_fin p)
+  if | w_reg <= (w_fin + 1) -> advance p
+     | otherwise -> putTMVar (_wait_fin p) (w_fin + 1)
+
 signal :: Enum p => Phaser p -> IO ()
-signal ph = modifyMVar_ (_arrived ph) (\i -> return (i+1))
+signal p = atomically $ do
+  w_reg <- readTVar  (_wait_reg p)
+  s_reg <- readTVar  (_sig_reg  p)
+  s_rx  <- takeTMVar (_sig_rx   p)
+  let is_last_signal = s_reg <= (s_rx + 1)
+      no_waits       = w_reg == 0
+      unblockWaits   = putTMVar (_wait_fin p) 0
+  if | is_last_signal && no_waits -> advance p
+     | is_last_signal             -> unblockWaits
+     | otherwise                  -> putTMVar (_sig_rx p) (s_rx + 1)
 
--- TODO: Consider adding "signalUntil" and "signalFor"
+-- Increase the phase to its successor.
+nextPhase :: Enum p => Phaser p -> STM ()
+nextPhase p = modifyTVar (_phase p) succ
 
-{- |
-  Don't count the thread among those arriving at the phaser, but don't
-  block waiting for any others to arrive. A thread that lurks on the @Phaser@
-  should not `signal` on the @Phaser@.
-
-  Be careful when using `lurk` - if the phaser advances before
-  lurk is called, it may cause `lurk` to wait for a cycle after the one we
-  actually care about. For this reason, it's probably safer to use `lurkUntil`
-  or `lurkFor`.
--}
-lurk :: Enum p => Phaser p -> IO ()
-lurk ph = do
-  block <- modifyMVar (_wake_queue ph) (sleepOn)
-  takeMVar block >> return ()
-
--- | Like `awaitFor`, but `lurk`s rather than `await`s.
-lurkFor :: Enum p => Int -> Phaser p -> IO ()
-lurkFor 0 ph = return ()
-lurkFor i ph = lurk ph >> lurkFor (i - 1) ph
-
--- | Like `awaitUntil`, but `lurk`s rather than `await`s.
-lurkUntil :: Enum p => Eq p => p -> Phaser p -> IO ()
-lurkUntil target_phase phaser =
-  phase phaser >>= (\current_phase -> do
-    when (current_phase /= target_phase) $
-      lurk phaser >> lurkUntil target_phase phaser
-    return ()
-  )
-
--- | Advance a @Phaser@ to the next phase. For internal use only.
-nextPhase :: Enum p => Phaser p -> IO ()
-nextPhase ph = modifyMVar_ (_phase ph) (return . succ)
-{-# INLINE nextPhase #-}
-
--- | Awaken all threads blocked on the phaser.
--- | For internal use only!
-awaken :: Phaser p -> IO ()
-awaken ph =
-  uninterruptibleMask_ $
-  modifyMVar_ (_wake_queue ph)
-    (\wq -> do
-       mapM ((flip putMVar) ()) wq
-       return []
-    )
+-- Move on to the next phase and permit entry into the phaser.
+advance :: Enum p => Phaser p -> STM ()
+advance p = nextPhase p >> putTMVar (_entered p) 0
